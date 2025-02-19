48bf5dfd0722a1fbbe2b0b170c13ac34c21c2dda
Falilen <qazwsxedc122880@gmail.com>
2025-02-19 12:14:30 +0300

    спринт1 финал

--- app/src/main/java/com/example/zh_matuale/MainActivity.kt
+++ app/src/main/java/com/example/zh_matuale/MainActivity.kt
@@ -1,31 +1,43 @@
 package com.example.zh_matuale
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
 import com.example.zh_matuale.navigation.AppNavHost
 import com.example.zh_matuale.ui.theme.ZhMatualeTheme
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ZhMatualeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    AppNavHost()
-                }
+                Scaffold(
+                    containerColor = Color(0xFF1A1919),
+                    content = { paddingValues ->
+                        Surface(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(paddingValues),
+                            color = Color(0xFF1A1919)
+                        ) {
+                            AppNavHost()
+                        }
+                    }
+                )
+
             }
         }
     }
