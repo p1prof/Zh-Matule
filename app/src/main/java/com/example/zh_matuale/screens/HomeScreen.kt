b89c409c16ed50b669fd79824891959375e4094a
Falilen <qazwsxedc122880@gmail.com>
2025-02-19 11:27:55 +0300

    first commit

---
+++ app/src/main/java/com/example/zh_matuale/screens/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.zh_matuale.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Text(text = "HOME SCREEN")
+}
---
+++ app/src/main/java/com/example/zh_matuale/screens/LogscScreen.kt
@@ -0,0 +1,245 @@
+package com.example.zh_matuale.screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.zh_matuale.navigation.NavRoute
+import com.example.zh_matuale.ui.theme.ZhMatualeTheme
+import com.example.zhdapp.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun LogScreen(navController: NavHostController) {
+    var email by remember { mutableStateOf("xyz@gmail.com") }
+    var password by remember { mutableStateOf("xyz123!!!") }
+    var isEmailValid by remember { mutableStateOf(true) }
+    var isPasswordValid by remember { mutableStateOf(true) }
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF7F7F9))
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 120.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                    text = "Привет!",
+                    textAlign = TextAlign.Center,
+                    fontSize = 36.sp,
+                    fontWeight = FontWeight.Medium,
+                    fontFamily = FontFamily(Font(R.font.newpeninimmt))
+                )
+
+                Text(
+                    text = "Заполните Свои Данные Или\nПродолжите Через Социальные Медиа",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding().fillMaxWidth(),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = FontFamily(Font(R.font.newpeninimmt)),
+                    color = Color(0xFF707B81)
+                )
+
+                Column(
+                    modifier = Modifier.padding(horizontal = 20.dp, vertical = 32.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Column {
+                        Text(
+                            text = "Email",
+                            textAlign = TextAlign.Start,
+                            fontSize = 16.sp,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+
+                        OutlinedTextField(
+                            value = email,
+                            onValueChange = {
+                                email = it
+                                isEmailValid = isValidEmail(it)
+                            },
+                            shape = RoundedCornerShape(20.dp),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 8.dp),
+                            colors = TextFieldDefaults.colors(
+                                focusedContainerColor = Color.LightGray,
+                                unfocusedContainerColor = Color.LightGray,
+                                focusedIndicatorColor = Color.LightGray,
+                                unfocusedIndicatorColor = Color.LightGray
+                            ),
+                            isError = !isEmailValid
+                        )
+                        if (!isEmailValid) {
+                            Text(
+                                text = "Некорректный email",
+                                color = Color.Red,
+                                fontSize = 12.sp,
+                                modifier = Modifier.align(Alignment.Start)
+                            )
+                        }
+                    }
+
+                    PasswordTextField(password) { newPassword ->
+                        password = newPassword
+                        isPasswordValid = newPassword.isNotEmpty()
+                    }
+
+                    if (!isPasswordValid) {
+                        Text(
+                            text = "Пароль не может быть пустым",
+                            color = Color.Red,
+                            fontSize = 12.sp,
+                            modifier = Modifier.align(Alignment.Start)
+                        )
+                    }
+
+                    Text(
+                        "Восстановить",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.End,
+                        fontSize = 14.sp,
+                        color = Color.Gray
+                    )
+
+                    Button(
+                        onClick = {
+                            if (isEmailValid && isPasswordValid && email.isNotEmpty() && password.isNotEmpty()) {
+                                navController.navigate(NavRoute.Home.route)
+                            } else {
+                                isEmailValid = isValidEmail(email)
+                                isPasswordValid = password.isNotEmpty()
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 32.dp)
+                            .height(50.dp)
+                            .align(Alignment.CenterHorizontally),
+                        shape = RoundedCornerShape(16.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFF48B2E7)
+                        )
+                    ) {
+                        Text("Войти")
+                    }
+                }
+            }
+
+            Text(
+                text = "Вы впервые? Создать пользователя",
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 12.dp),
+                fontSize = 14.sp
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordTextField(password: String, onPasswordChange: (String) -> Unit) {
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.padding(vertical = 8.dp)
+    ) {
+        Text(
+            text = "Пароль",
+            textAlign = TextAlign.Start,
+            fontSize = 16.sp,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = onPasswordChange,
+            shape = RoundedCornerShape(20.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.LightGray,
+                unfocusedContainerColor = Color.LightGray,
+                focusedIndicatorColor = Color.LightGray,
+                unfocusedIndicatorColor = Color.LightGray
+            ),
+            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                    Icon(
+                        imageVector = if (isPasswordVisible) Icons.Filled.FavoriteBorder else Icons.Filled.Favorite,
+                        contentDescription = if (isPasswordVisible) "Скрыть пароль" else "Показать пароль",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
+        )
+    }
+}
+
+fun isValidEmail(email: String): Boolean {
+    val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"
+    return email.matches(emailRegex.toRegex())
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PrevLogScreen() {
+    ZhMatualeTheme {
+        LogScreen(navController = rememberNavController())
+    }
+}
